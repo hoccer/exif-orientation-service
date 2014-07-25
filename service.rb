60694ce3fb0dcb2d356410c30a09f1f36edf2a03
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-25 16:22:04 +0200

    Use rmagick to fix image orientation

--- service.rb
+++ service.rb
@@ -1,5 +1,10 @@
+require 'rmagick'
 require 'sinatra'
 
-get '*' do |path|
-  "Hello #{path}"
+get '/*' do |path|
+  img = Magick::Image.read(path)[0]
+  img.auto_orient!
+
+  content_type img.mime_type
+  img.to_blob
 end
