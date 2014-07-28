a549f161eca714d6a5951bcf86df2231865e4e07
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-28 08:00:05 +0200

    Look for images in ./images folder

--- service.rb
+++ service.rb
@@ -1,14 +1,20 @@
 require 'rmagick'
 require 'sinatra'
 
+configure do
+  set :image_path, File.expand_path('../images', __FILE__)
+end
+
 get '/*' do |path|
+  file_path = File.join(settings.image_path, path)
+
   begin
-    img = Magick::Image.read(path)[0]
+    img = Magick::Image.read(file_path)[0]
     img.auto_orient!
 
     content_type img.mime_type
     img.to_blob
   rescue Magick::ImageMagickError
-    send_file path
+    send_file file_path
   end
 end
