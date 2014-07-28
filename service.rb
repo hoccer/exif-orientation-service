0c2286a147f5e1b8f9fc3edf3802adf1fec5250b
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-28 07:54:32 +0200

    Handle files that aren't images and errors

--- service.rb
+++ service.rb
@@ -2,9 +2,13 @@ require 'rmagick'
 require 'sinatra'
 
 get '/*' do |path|
-  img = Magick::Image.read(path)[0]
-  img.auto_orient!
+  begin
+    img = Magick::Image.read(path)[0]
+    img.auto_orient!
 
-  content_type img.mime_type
-  img.to_blob
+    content_type img.mime_type
+    img.to_blob
+  rescue Magick::ImageMagickError
+    send_file path
+  end
 end
